c8bd2f21b2ac962ebebf3d92353069ba9786cb7f
phimmedia <65354228+phimmedia@users.noreply.github.com>
2020-07-14 21:10:28 -0700

    Update mahoa.php

--- mahoa/mahoa.php
+++ mahoa/mahoa.php
@@ -1,4 +1,48 @@
 <?php
+session_start();
+/* DECLARE VARIABLES */
+$username = 'admin';
+$password = 'admin';
+$random1 = 'secret_key1';
+$random2 = 'secret_key2';
+$hash = md5($random1 . $password . $random2);
+$self = $_SERVER['REQUEST_URI'];
+/* USER LOGOUT */
+if(isset($_GET['logout']))
+{
+	unset($_SESSION['login']);
+}
+/* USER IS LOGGED IN */
+if (isset($_SESSION['login']) && $_SESSION['login'] == $hash)
+{
+	logged_in_msg($username);
+}
+/* FORM HAS BEEN SUBMITTED */
+else if (isset($_POST['submit']))
+{
+	if ($_POST['username'] == $username && $_POST['password'] == $password)
+	{
+		//IF USERNAME AND PASSWORD ARE CORRECT SET THE LOGIN SESSION
+		$_SESSION["login"] = $hash;
+		header("Location: $_SERVER[PHP_SELF]");
+	}
+	else
+	{
+		// DISPLAY FORM WITH ERROR
+		display_login_form();
+		display_error_msg();
+	}
+}
+/* SHOW THE LOGIN FORM */
+else
+{
+	display_login_form();
+}
+/* TEMPLATES */
+function display_login_form()
+{
+?>
+<?php
 include('function/function.php');
 
 	if($_POST['text'] || $_POST['key']){
