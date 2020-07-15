938cef5e0fd8a1751e263a5566fb07c40541281d
phimmedia <65354228+phimmedia@users.noreply.github.com>
2020-07-15 04:00:50 -0700

    Update get.php

--- api/get.php
+++ api/get.php
@@ -1,3 +1,5 @@
+<?php 	if (stristr($_SERVER['HTTP_REFERER'],"your otherdomain add here") || stristr($_SERVER['HTTP_REFERER'],"your otherdomain add here")|| stristr($_SERVER['HTTP_REFERER'],"your otherdomain add here")|| stristr($_SERVER['HTTP_REFERER'],"your otherdomain add here")) { ;
+?>
 <?php 
 function decode($string)
 {
@@ -36,3 +38,4 @@ jwplayer().resize($(window).width(),$(window).height())
 </script></body></html>
 
 
+<?php } else echo 'Opps sorry! for demo only, thanks. For more info contact us http://bundlecodes.com/'; ?>	
