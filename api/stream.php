48a5e8ecb629ea1bf6b4cc81879f51e81752d425
phimmedia <65354228+phimmedia@users.noreply.github.com>
2020-07-16 07:49:47 +0700

    Edit

--- api/stream.php
+++ api/stream.php
@@ -1,8 +1,4 @@
 
-<?php 	if (stristr($_SERVER['HTTP_REFERER'],"dalatplay.xyz") || stristr($_SERVER['HTTP_REFERER'],"dalatplay.blogspot.com")||
-stristr($_SERVER['HTTP_REFERER']," ")||
- stristr($_SERVER['HTTP_REFERER'],"embedapi.herokuapp.com/vast15s")|| stristr($_SERVER['HTTP_REFERER'],"your otherdomain add here")) { ;?>
-
 
 <?php 
 include("decode.php");
@@ -13,7 +9,3 @@ header('Location: '.openssl($file));
 ?>
 
 
-<?php
-}
-else echo 'Opps sorry!  For more info contact us ';
-?>	
