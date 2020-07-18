468cf15951c15831c962afbf92d524e0b509480d
phimmedia <65354228+phimmedia@users.noreply.github.com>
2020-07-18 05:06:00 -0700

    Update stream.php

--- api/stream.php
+++ api/stream.php
@@ -1,8 +1,10 @@
 <?php
-if(md5($_SESSION["check"])=="aaa"){
-      include("decode.php");
+include("decode.php");
 $link = $_GET['api'];
 $file = $link;
+if(md5($_SESSION["check"])=="aaa"){
+      
+
 header('Location: '.openssl($file));
     }else{
 
