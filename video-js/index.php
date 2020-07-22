6d426b5565d89ad58fdff8528bbc394e1c063412
phimmedia <65354228+phimmedia@users.noreply.github.com>
2020-07-22 16:14:29 +0700

    Add files via upload

---
+++ video-js/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Video.js Demo</title>
+  
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<html>
+  <head>
+    <!-- Load dependent stylesheets. -->
+    <link rel="stylesheet" href="//googleads.github.io/videojs-ima/node_modules/video.js/dist/video-js.min.css" />
+    <link rel="stylesheet" href="//googleads.github.io/videojs-ima/node_modules/videojs-contrib-ads/dist/videojs.ads.css" />
+    <link rel="stylesheet" href="//googleads.github.io/videojs-ima/dist/videojs.ima.css" />
+  </head>
+
+  <body>
+    <video id="content_video" class="video-js vjs-default-skin"
+        controls preload="auto" width="640" height="360">
+      <source src="<?=$_GET['id']?>"
+          type="video/mp4" />
+    </video>
+    <!-- Load dependent scripts -->
+    <script src="//googleads.github.io/videojs-ima/node_modules/video.js/dist/video.min.js"></script>
+    <script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>
+    <script src="//googleads.github.io/videojs-ima/node_modules/videojs-contrib-ads/dist/videojs.ads.min.js"></script>
+    <script src="//googleads.github.io/videojs-ima/dist/videojs.ima.js"></script>
+  </body>
+</html>
+<!-- partial -->
+  <script  src="./script.js"></script>
+
+</body>
+</html>
