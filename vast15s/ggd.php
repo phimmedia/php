7ab9ee7b951520754bfb20724bd5e3733940c0a7
phimmedia <65354228+phimmedia@users.noreply.github.com>
2020-07-28 16:59:32 +0700

    Update ggd.php

--- vast15s/ggd.php
+++ vast15s/ggd.php
@@ -1,3 +1,6 @@
+<?php 	if (stristr($_SERVER['HTTP_REFERER'],"dalatplay.xyz") || stristr($_SERVER['HTTP_REFERER'],"dalatplay.blogspot.com")||
+stristr($_SERVER['HTTP_REFERER']," ")||
+ stristr($_SERVER['HTTP_REFERER'],"embedapi.herokuapp.com/vast15s")|| stristr($_SERVER['HTTP_REFERER'],"your otherdomain add here")) { ;?>
 <meta charset="UTF-8">
 <center>
 <div style="width: 100%px; height: 100%px; position: relative;">
@@ -65,3 +68,7 @@ document.onmousedown=disableselect
 document.onclick=reEnable
 }
 </script>
+<?php
+}
+else echo 'Opps sorry!  For more info contact us ';
+?>
